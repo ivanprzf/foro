20be903753f2050ebe2cb0af1cc25312fe06add0
Ivan Perez Fuentes <ivanprzf@gmail.com>
2016-11-17 17:18:00 +0100

    Foro completo
    Ficheros completos

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'noforo',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'nf',
+    'strict' => false,
+  ),
+  'url' => 'http://localhost/foro',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
